8bfe0c4861e597232a2eb4d481be720ee7787316
Mahala444 <jmj42@usf.edu>
2025-06-17 13:53:52 -0500

    Created Employee Data

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+const employees = [
+  { name: "Alice", hourlyRate: 25, hoursWorked: 38 },
+  { name: "Bob", hourlyRate: 20, hoursWorked: 45 },
+  { name: "Charlie", hourlyRate: 30, hoursWorked: 50 }
+];
